f029f6fb3fef3a4d2423d98fa5911402603f1a74
John-Vincent-Rabaca <johnvincentrabaca0@gmail.com>
2025-12-28 21:52:31 +0800

    ADded function

--- sari-sari-store.py
+++ sari-sari-store.py
@@ -1,4 +1,24 @@
 #Sari-sari Store py
+def print_category(category):
+    if category == "Noodles":
+        for noodle in categories["Noodles"]:
+            print(noodle)
+    elif category == "Canned Products":
+        for noodle in categories["Canned Products"]:
+            print(noodle)
+    elif category == "Biscuits":
+        for noodle in categories["Biscuits"]:
+            print(noodle)
+    elif category == "Beverages":
+        for noodle in categories["Beverages"]:
+            print(noodle)
+    elif category == "Shampoo & Conditioners":
+        for noodle in categories["Shampoo & Conditioners"]:
+            print(noodle)
+    elif category == "Laundry Products":
+        for noodle in categories["Laundry Products"]:
+            print(noodle)
+
 
 categories = {
     "Noodles": {"lucky me": 11.00, 
@@ -16,24 +36,7 @@ for category in categories:
 category = input("\nChoose a category: ").title()
 print()
 
-if category == "Noodles":
-    for noodle in categories["Noodles"]:
-        print(noodle)
-elif category == "Noodles":
-    for noodle in categories["Noodles"]:
-        print(noodle)
-elif category == "Noodles":
-    for noodle in categories["Noodles"]:
-        print(noodle)
-elif category == "Noodles":
-    for noodle in categories["Noodles"]:
-        print(noodle)
-elif category == "Noodles":
-    for noodle in categories["Noodles"]:
-        print(noodle)
-elif category == "Noodles":
-    for noodle in categories["Noodles"]:
-        print(noodle)
+print_category(category)
     
 item = input("\nItem: ")
 print(f"Price for {item} is: {categories[category][item]}")
